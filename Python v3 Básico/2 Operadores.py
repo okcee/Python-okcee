9276f3b6261ce129d5a500a5dae147d56055a10e
David Saavedra Varela <okcee@hotmail.com>
2025-03-05 13:12:49 +0100

    d050325 .2 T5C9

--- Python v3 Básico/2 Operadores.py
+++ Python v3 Básico/2 Operadores.py
@@ -106,3 +106,59 @@ if ( 20 <= edad < 30) or (30 <= edad <40):
 #        print('Fuera de rango')
 else:
     print("No está dentro de los 20's ni 30's")
+
+# Módulo. Nos devuelve el resto de una división:
+n_numerador = 85
+n_denominador = 9
+n_resto = n_numerador%n_denominador
+print("El resto de dividir" , n_numerador , "entre" , n_denominador , "es" , n_resto)
+
+# == Igual que...
+# No confundir con el operador de asignación =
+# Con = le damos un valor a una variable. Con == comprobamos si dos objetos son iguales.
+n_numero1 = 34
+s_texto1 = "34"
+n_numero1 == s_texto1
+n_numero2 = 34
+n_numero3 = 34
+n_numero2 == n_numero3
+
+# != Diferente que...
+n_numero4 = 34
+n_numero5 = 34
+n_numero4 != n_numero5
+
+# += Suma e incremento
+n_numero6 = 34
+n_numero6 += 1 #Sería como poner:
+n_numero6 = n_numero6 +1
+print(n_numero6)
+
+'''Operadores lógicos'''
+a = True
+b = False
+resultado = a and b
+# print(resultado)
+resultado = a or b
+# print(resultado)
+resultado = not a
+print(resultado)
+
+#----------------------------------
+
+# Sintaxis simplificada para varios operadores lógicos
+edad = int(input('Introduce tu edad: '))
+#veintes = edad >= 20 and edad < 30
+#print(veintes)
+#treintas = edad >= 30 and edad <40
+#print(treintas)
+if ( 20 <= edad < 30) or (30 <= edad <40):
+    print('Dentro de rango (20\'s) o (30\'s)')
+# if veintes:
+# print('Dentro de los 20\'s')
+# elif treintas:
+# print('Dentro de los 30\'s')
+# else:
+# print('Fuera de rango')
+else:
+    print("No está dentro de los 20's ni 30's")
--- Python v3 Básico/4.1 Entrada de datos.py
+++ Python v3 Básico/4.1 Entrada de datos.py
@@ -1,3 +1,11 @@
+s_nombreIntroducido = input("Introduzca su nombre: ")
+print("Bienvenido", s_nombreIntroducido)
+# -------------------------------------
+""" IMPORTANTE: Todo lo introducido por input() se considera string, aunque sea un número,
+por lo que, si necesitamos operar matemáticamente con números, tendremos que hacer un casting:
+"""
+s_edad = int(input("Introduzca su edad: "))
+print("El año que viene tendrá usted ", s_edad + 1, "años")
 
 # Ejemplo de introducir datos utilizando append() para listas y add() para conjuntos.
 lista = list()
