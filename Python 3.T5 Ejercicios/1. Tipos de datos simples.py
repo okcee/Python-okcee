93590b6bb36bf415b4e4590a7e1de8355b26e677
David Saavedra Varela <okcee@hotmail.com>
2025-03-09 17:37:14 +0100

    d090325 .4 e1

---
+++ Python 3.T5 Ejercicios/1. Tipos de datos simples.py
@@ -0,0 +1,22 @@
+'''Ejercicio 1
+Escribir un programa que muestre por pantalla la cadena ¡Hola Mundo!.'''
+print ("¡Hola Mundo!")
+
+'''Ejercicio 2
+Escribir un programa que almacene la cadena ¡Hola Mundo! en una variable y luego muestre por pantalla el contenido de la variable.'''
+s_var="¡Hola Mundo!"
+print(s_var)
+
+'''Ejercicio 3
+Escribir un programa que pregunte el nombre del usuario en la consola y después de que el usuario lo introduzca muestre por pantalla la cadena ¡Hola <nombre>!, donde <nombre> es el nombre que el usuario haya introducido'''
+s_name=input("Por favor, introduce tu nombre: ") # Creo una variable
+print("¡Hola ",s_name,"!") # Imprimo strings y la variable
+
+'''Ejercicio 4
+Escribir un programa que muestre por pantalla el resultado de la siguiente operación aritmética 
+((3+2)/(2*5))^2'''
+f_var=((3+2)/(2*5))**2
+print(f_var)
+
+'''Ejercicio 5
+Escribir un programa que pregunte al usuario por el número de horas trabajadas y el coste por hora. Después debe mostrar por pantalla la paga que le corresponde.'''
