bf481c00d0f463e52879d04f664b62c8e7d28d2f
David Saavedra Varela <okcee@hotmail.com>
2025-03-10 20:39:03 +0100

    d100325

--- Python 3.T5 Ejercicios/1. Condicionales.py
+++ Python 3.T5 Ejercicios/1. Condicionales.py
@@ -1,2 +1,10 @@
 '''Ejercicio 1
 Escribir un programa que pregunte al usuario su edad y muestre por pantalla si es mayor de edad o no.'''
+
+try:
+    numero = int(input("Introduce un número entero: "))
+    print("El número introducido es:", numero)
+    suma = (numero*(numero+1))/2
+    print("La suma de los números primeros enteros es:", numero)
+except ValueError:
+    print("Error: Debes introducir un número entero válido.")
